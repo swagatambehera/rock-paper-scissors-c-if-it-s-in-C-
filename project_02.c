49ca81c0b1f27e0a748c6c1cd84d0c3185271e02
Swagatam Behera <swagatam.behera.77@gmail.com>
2025-09-19 22:24:49 +0530

    Update project_02.c

--- project_02.c
+++ project_02.c
@@ -12,12 +12,25 @@ int main() {
     printf("Enter your choice:\n1. Rock\n2. Paper\n3. Scissors\n");
     scanf("%d", &player);
 
-    if (player== 1) printf("You chose Rock\n");
-    else if (player == 2) printf("You chose Paper\n");
-    else if (player == 3) printf("You chose Scissors\n");
-    if (computer == 1) printf("Computer chose Rock\n");
-    else if (computer == 2) printf("Computer chose Paper\n");
-    else if (computer == 3) printf("Computer chose Scissors\n");
+    if (player== 1) {
+        printf("You chose Rock\n");
+    }
+    else if (player == 2) {
+        printf("You chose Paper\n");
+    }
+    else if (player == 3) {
+        printf("You chose Scissors\n");
+    }
+    
+    if (computer == 1){
+        printf("Computer chose Rock\n");
+    }
+    else if (computer == 2){
+        printf("Computer chose Paper\n");
+    }
+    else if (computer == 3) {
+        printf("Computer chose Scissors\n");
+    }
 
     if (player == computer) {
         printf("It's a draw!\n");
@@ -25,9 +38,11 @@ int main() {
                (player == 2 && computer == 1) ||
                (player == 3 && computer == 2)) {
         printf("You win!\n");
-    } else {
+    }
+    else {
         printf("Computer wins!\n");
     }
 
     return 0;
 }
+
